c090018b989e2f353b4a3ccb5262f5fba0c351b8
Julian <julivizi@yahoo.de>
2024-09-17 22:43:16 +0200

    push

--- Assets/_Scripts/Prototyping/VectorBuilding.cs
+++ Assets/_Scripts/Prototyping/VectorBuilding.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace OutcastMayor
@@ -23,6 +24,16 @@ namespace OutcastMayor
 
         public Shapes.Line currentLine;
 
+
+        [Header("Rectangle Tool")]
+        ///State of the rectangle building. 0 = No point placed, 1= start point placed, 2 = first line placed
+        public int rectState = 0;
+        public Shapes.Rectangle currentRectangle;
+        public Transform currentRectangleParent;
+        public VectorPoint rectP1,rectP2,rectP3,rectP4;
+
+
+        [Header("Graph")]
         public VectorPointGraph currentVectorPointGraph;
 
         public List<VectorPointGraph> vectorPointGraphs = new List<VectorPointGraph>();        
@@ -44,16 +55,38 @@ namespace OutcastMayor
         void Update()
         {
             Vector3 mousePosition = inputManager.HitInfo.point;
-            if(lastLinePoint != null)
+            if(buildMode == BuildMode.Line)
             {
-                currentLine.End = currentLine.transform.InverseTransformPoint(mousePosition);
+                if(lastLinePoint != null)
+                {
+                    currentLine.End = currentLine.transform.InverseTransformPoint(mousePosition);
+                }
+            }            
+            else if(buildMode == BuildMode.Rectangle)
+            {
+                if(rectState == 0)
+                {                    
+                    currentRectangle.Width = .1f;
+                    currentRectangle.Height = .1f;
+                    currentRectangleParent.transform.LookAt(rectP2.worldPosition, Vector3.up);
+                }
+                else if(rectState == 1)
+                {
+                    currentRectangleParent.transform.LookAt(mousePosition, Vector3.up);
+                    currentRectangle.Width = Vector3.Distance(rectP1.worldPosition, mousePosition);
+                    currentRectangle.Height = 0.1f;
+                }
+                else if(rectState == 2)
+                {
+                    currentRectangle.Height = Vector3.Distance(rectP2.worldPosition, mousePosition);
+                }
             }
-
         }
 
         /// <summary>
         /// Place a continuous stream of linepoints
         /// </summary>
+        [Button]
         public void ChangeToLineMode()
         {
             buildMode = BuildMode.Line;
@@ -62,6 +95,7 @@ namespace OutcastMayor
         /// <summary>
         /// Place a rectangle with 4 points
         /// </summary>
+        [Button]
         public void ChangeToRectangleMode()
         {
             buildMode = BuildMode.Rectangle;
@@ -82,6 +116,7 @@ namespace OutcastMayor
 
             ControlPoint clickedPoint = inputManager.HitInfo.collider.GetComponent<ControlPoint>();
 
+            #region LineBuilding
             //Just place another point and add it to the last placed linepoint
             if(buildMode == BuildMode.Line)
             {
@@ -124,9 +159,6 @@ namespace OutcastMayor
                         currentLine.Color = currentVectorPointGraph.graphColor;
                     }
 
-                    
-                    Debug.Log("[VectorBuilding] Add Point normally");
-
                     VectorPoint newPoint = new VectorPoint(clickPosition, lastLinePoint, currentVectorPointGraph);
                     if(lastLinePoint != null)
                         lastLinePoint.AddPoint(newPoint);
@@ -135,19 +167,114 @@ namespace OutcastMayor
                     
 
                     //Also add a controlPoint
-                    ControlPoint newControlPoint = Instantiate(controlPoint, transform);
-                    newControlPoint.transform.position = clickPosition;
-                    newControlPoint.SetData(newPoint, this);
-                    AddControlPoint(newControlPoint);
+                    AddControlPoint(newPoint);
                 }
-                
                 currentLine.Start = currentLine.transform.InverseTransformPoint(lastLinePoint.worldPosition);
+            }
+            #endregion
+            if(buildMode == BuildMode.Rectangle)
+            {
+                if(clickedPoint != null)
+                {
+                    if(currentVectorPointGraph == null)
+                    {
+                        currentVectorPointGraph = clickedPoint.vectorPoint.vectorPointGraph;
+                        currentRectangle.Color = currentVectorPointGraph.graphColor;
+                    }
+
+                    if(rectState == 0)
+                    {
+                        rectP1 = clickedPoint.vectorPoint;
+                        currentRectangleParent.position = clickedPoint.vectorPoint.worldPosition;
+                        rectState = 1;
+                    }
+                    else if(rectState == 1)
+                    {
+                        rectP2 = clickedPoint.vectorPoint;
+                        rectP1.AddPoint(rectP2);
+                        currentRectangleParent.transform.LookAt(rectP2.worldPosition, Vector3.up);
+                        currentRectangle.Width = Vector3.Distance(rectP1.worldPosition, rectP2.worldPosition);
+                        rectState = 2;
+                    }
+                    else if(rectState == 2)
+                    {
+                        rectP3 = new VectorPoint(clickPosition, rectP2, currentVectorPointGraph);
+                        rectP2.AddPoint(rectP3);
+
+                        rectP4 = new VectorPoint(rectP3.worldPosition + (rectP1.worldPosition - rectP2.worldPosition), rectP3, currentVectorPointGraph);
+                        rectP1.AddPoint(rectP4);
+
+                        currentRectangle.Width = Vector3.Distance(rectP2.worldPosition, rectP3.worldPosition);
+
+                        //Add finished points
+                        
+                        currentVectorPointGraph.AddPoint(rectP1);
+                        currentVectorPointGraph.AddPoint(rectP2);
+                        currentVectorPointGraph.AddPoint(rectP3);
+                        currentVectorPointGraph.AddPoint(rectP4);
+                        AddControlPoint(rectP1);
+                        AddControlPoint(rectP2);
+                        AddControlPoint(rectP3);
+                        AddControlPoint(rectP4);
+                        rectState = 0;
+                        currentVectorPointGraph = null;
+                    }
+                }
+                else
+                {
+                    if(currentVectorPointGraph == null)
+                    {
+                        currentVectorPointGraph = new VectorPointGraph();
+                        vectorPointGraphs.Add(currentVectorPointGraph);
+                        currentRectangle.Color = currentVectorPointGraph.graphColor;
+                    }
+                    if(rectState == 0)
+                    {
+                        rectP1 = new VectorPoint(clickPosition, null, currentVectorPointGraph);
+                        currentRectangleParent.position = clickPosition;
+                        rectState = 1;
+                    }
+                    else if(rectState == 1)
+                    {
+                        rectP2 = new VectorPoint(clickPosition, rectP1, currentVectorPointGraph);
+                        rectP1.AddPoint(rectP2);
+                        currentRectangleParent.transform.LookAt(rectP2.worldPosition, Vector3.up);
+                        currentRectangle.Width = Vector3.Distance(rectP1.worldPosition, rectP2.worldPosition);
+                        rectState = 2;
+                    }
+                    else if(rectState == 2)
+                    {
+                        rectP3 = new VectorPoint(clickPosition, rectP2, currentVectorPointGraph);
+                        rectP2.AddPoint(rectP3);
+
+                        rectP4 = new VectorPoint(rectP3.worldPosition + (rectP1.worldPosition - rectP2.worldPosition), rectP3, currentVectorPointGraph);
+                        rectP1.AddPoint(rectP4);
+
+                        currentRectangle.Width = Vector3.Distance(rectP2.worldPosition, rectP3.worldPosition);
+
+                        //Add finished points
+                        
+                        currentVectorPointGraph.AddPoint(rectP1);
+                        currentVectorPointGraph.AddPoint(rectP2);
+                        currentVectorPointGraph.AddPoint(rectP3);
+                        currentVectorPointGraph.AddPoint(rectP4);
+                        AddControlPoint(rectP1);
+                        AddControlPoint(rectP2);
+                        AddControlPoint(rectP3);
+                        AddControlPoint(rectP4);
+                        rectState = 0;
+                        currentVectorPointGraph = null;
+                    }
+                }
             }
         }
 
-        public void AddControlPoint(ControlPoint _point)
+        public void AddControlPoint(VectorPoint _point)
         {
-            controlPoints.Add(_point);
+            ControlPoint newControlPoint = Instantiate(controlPoint, transform);
+            newControlPoint.transform.position = _point.worldPosition;
+            newControlPoint.SetData(_point, this);
+            controlPoints.Add(newControlPoint);
         } 
     }
 
